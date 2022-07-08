cd71141c971ad99c5fc5ffebe43056c64619af3f
sasa5680 <sasa5680@naver.com>
2022-07-09 00:07:53 +0900

    TEST

--- src/main/java/com/example/hello/Controller/TestController.java
+++ src/main/java/com/example/hello/Controller/TestController.java
@@ -25,7 +25,7 @@ public class TestController {
     @Operation(summary = "test hello", description = "hello api example")
     @GetMapping("/test")
     public String test(){
-        return "password" + mailService.getPass();
+        return "password" + mailService.getPass()+ "123456";
     }
 
     //auth 테스트용
