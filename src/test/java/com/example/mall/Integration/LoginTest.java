3051d5b27647fda9c82864f3fe190261efd7377f
sasa5680 <sasa5680@naver.com>
2022-08-23 20:24:48 +0900

    dd

--- src/test/java/com/example/mall/Integration/LoginTest.java
+++ src/test/java/com/example/mall/Integration/LoginTest.java
@@ -1,4 +1,24 @@
 package com.example.mall.Integration;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class LoginTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    public void peTest() {
+        String pw = "123456";
+
+        boolean result = passwordEncoder.matches(pw, passwordEncoder.encode(pw));
+
+        Assert.assertEquals(true, result);
+    }
 }
